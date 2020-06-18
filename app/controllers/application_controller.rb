0f393e87ead4ac0b7e0f67235985e88f9d7b6b4d
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-18 23:43:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       @team = Team.new(params[:team])
         params[:team][:members].each {|details| SuperHero.new(details)}
         @super_heroes = SuperHero.all
-        
+
         erb :team
     end
 
