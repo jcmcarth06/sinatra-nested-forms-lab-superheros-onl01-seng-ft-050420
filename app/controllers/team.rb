731d072630e6676a7eed5dd9cbbf31632e9edb83
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-18 23:27:26 +0000

    Automatically backed up by Learn

---
+++ app/controllers/team.rb
@@ -0,0 +1,41 @@
+class Team
+  attr_accessor :name, :type, :booty
+
+  @@class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
+end = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
+end
